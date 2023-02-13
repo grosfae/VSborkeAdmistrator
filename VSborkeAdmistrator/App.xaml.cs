19b5c3aa3f39bbf901c14fac3886eb09903f9013
Кирилл Лосев <grosfae@outlook.com>
2023-02-13 10:02:08 +0300

    Добавьте файлы проекта.

---
+++ VSborkeAdmistrator/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using VSborkeAdmistrator.Components;
+
+namespace VSborkeAdmistrator
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static VSborkeBaseEntities DB = new VSborkeBaseEntities();
+        public static User LoggedUser;
+    }
+}
---
+++ VSborkeAdmistrator/Pages/MenuPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VSborkeAdmistrator.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+        private void ProfileBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void AccountListBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ComputerListBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void InfoShowBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ConfigBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ExitBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
